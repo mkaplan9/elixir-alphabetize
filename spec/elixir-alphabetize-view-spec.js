dad0a91a823ea2fe12468cf6a950228a96a97243
Max Kaplan <max.m.kaplan@gmail.com>
2017-01-16 15:53:12 -0500

    Second commit

---
+++ spec/elixir-alphabetize-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ElixirAlphabetizeView from '../lib/elixir-alphabetize-view';
+
+describe('ElixirAlphabetizeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
